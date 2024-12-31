10412d16c04fc99d953e5121c86cb09a3130a685
Steven McKenzie <sdmckenzie04@gmail.com>
2024-12-31 13:33:09 -0500

    Add constants.py and .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from constants import *
 import pygame
 
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
