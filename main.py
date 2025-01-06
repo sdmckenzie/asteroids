5e4ebf2ba63b942c4da12bfdbc8b48dd1cbce972
Steven McKenzie <sdmckenzie04@gmail.com>
2025-01-06 13:04:53 -0500

    Add screen and update .gitignore
    I added a screen object and attempted to update the .gitignore file to
    keep from copying my python venv data to github. So far this is
    unsuccessful; all of the venv files are trying to sync with the
    commit.

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import pygame
 
 
 def main():
+    pygame.init()
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.display()
 
 
 if __name__ == "__main__":
